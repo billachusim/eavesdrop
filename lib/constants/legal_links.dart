4b46d1ebe549e20120541bbdba093f403d93eb96
Tech Faculty <thetechfaculty@gmail.com>
2026-02-17 14:26:46 +0100

    iOS Archived And Uploaded Successfully To App Review

--- lib/constants/legal_links.dart
+++ lib/constants/legal_links.dart
@@ -2,5 +2,5 @@ class LegalLinks {
   static const String termsOfUse =
       'https://www.apple.com/legal/internet-services/itunes/dev/stdeula/';
   static const String privacyPolicy =
-      'https://sites.google.com/view/claire-diary/claire-privacy-policy';
+      'https://sites.google.com/view/claire-diary/eavesdrop-privacy-policy-and-terms-of-use';
 }
